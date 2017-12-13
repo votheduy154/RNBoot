a26b0229a71e402d4589685e1aa3c3342f82fbcc
DuyNguyen <duynguyen@DuyNguyens-Mac.local>
2017-12-13 07:50:11 +0700

    wix navigator

--- android/app/src/main/java/com/rnboot/MainActivity.java
+++ android/app/src/main/java/com/rnboot/MainActivity.java
@@ -1,15 +1,15 @@
 package com.rnboot;
-
+import com.reactnativenavigation.controllers.SplashActivity;
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "RNBoot";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "RNBoot";
+    // }
 }
